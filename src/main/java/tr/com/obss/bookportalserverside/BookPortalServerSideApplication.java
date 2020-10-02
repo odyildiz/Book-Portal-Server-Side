1b4b4d57581543e35b4b5e9ac634e3d57daee041
Özgür Doğan Yıldız <ozgurdoganyildiz@hotmail.com>
2020-10-02 18:11:40 +0300

    First commit

---
+++ src/main/java/tr/com/obss/bookportalserverside/BookPortalServerSideApplication.java
@@ -0,0 +1,13 @@
+package tr.com.obss.bookportalserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPortalServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPortalServerSideApplication.class, args);
+	}
+
+}
